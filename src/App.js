e6b68549f13b7dbff581834a81338f27542efcb2
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-04 15:47:11 +0530

    Setup Routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages
+import Dashboard from './pages/dashboard/Dashboard';
+import Create from './pages/create/Create';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Project from './pages/project/Project';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/projects/:id' element={<Project />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
