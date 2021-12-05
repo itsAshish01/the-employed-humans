08499ccfb7c81090bff0b9d4827ae2a025bcc396
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-05 12:11:35 +0530

    Bulit the signup page.

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,53 @@
+import { useState } from 'react';
 import './Signup.css';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className='auth-form'>
+      <h2>Create account</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          required
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <label>
+        <span>profile thumbnail:</span>
+        <input type='file' required />
+      </label>
+
+      <button className='btn'>Create account</button>
+    </form>
+  );
 };
 
 export default Signup;
