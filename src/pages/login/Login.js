12a7f0d33f5b3b480333eccd402a9ff3983e006f
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-07 19:49:26 +0530

    Build the login screen.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,51 @@
+import { useState } from 'react';
 import './Login.css';
+import { useLogin } from '../../hooks/useLogin';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { isPending, error, login } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Sign in</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button>Sign in</button>
+
+      {/* {!isPending && <button className='btn'>Sign in</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          Signing in...
+        </button>
+      )}
+      {error && <div className='error'>{error}</div>} */}
+    </form>
+  );
 };
 
 export default Login;
