d4a8b1082d3464851e0c13e10b471fe1347d7d7f
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-07 21:15:21 +0530

    Setup redirects & routes guard.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // pages
 import Dashboard from './pages/dashboard/Dashboard';
@@ -9,23 +9,68 @@ import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Sidebar />
-        <div className='container'>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/create' element={<Create />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/projects/:id' element={<Project />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className='container'>
+            <Navbar />
+            <Routes>
+              <Route
+                path='/'
+                element={
+                  <>
+                    {user && <Dashboard />}
+                    {!user && <Navigate to='/login' />}
+                  </>
+                }
+              />
+              <Route
+                path='/create'
+                element={
+                  <>
+                    {user && <Create />}
+                    {!user && <Navigate to='/login' />}
+                  </>
+                }
+              />
+              <Route
+                path='/projects/:id'
+                element={
+                  <>
+                    {user && <Project />}
+                    {!user && <Navigate to='/login' />}
+                  </>
+                }
+              />
+              <Route
+                path='/login'
+                element={
+                  <>
+                    {!user && <Login />}
+                    {user && <Navigate to='/' />}
+                  </>
+                }
+              />
+              <Route
+                path='/signup'
+                element={
+                  <>
+                    {!user && <Signup />}
+                    {user && <Navigate to='/' />}
+                  </>
+                }
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
