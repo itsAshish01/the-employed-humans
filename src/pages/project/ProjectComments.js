c687b733c8c211ef84a0f3891c5016f556ab7d20
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-08 20:31:21 +0530

    Rendering comments.

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Avatar from '../../components/Avatar';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
@@ -28,6 +29,25 @@ const ProjectComments = ({ project }) => {
   return (
     <div className='project-comments'>
       <h4>Project comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='comment-author'>
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className='comment-date'>
+                <p>Date here</p>
+              </div>
+              <div className='comment-content'>
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <form className='add-comment' onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
