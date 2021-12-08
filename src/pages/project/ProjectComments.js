8d833cbc4343fdae4d7fc1af666b5c4f48f8c464
Ashish Sharma <developer.ashish18@gmail.com>
2021-12-08 20:19:09 +0530

    Adding comments to firebase.

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFirestore } from '../../hooks/useFirestore';
 
-const ProjectComments = () => {
+const ProjectComments = ({ project }) => {
   const [newComment, setNewComment] = useState('');
   const { user } = useAuthContext();
+  const { updateDocument, response } = useFirestore('projects');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,7 +19,10 @@ const ProjectComments = () => {
       id: Math.random(),
     };
 
-    console.log(commentToAdd);
+    await updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
+    if (!response.error) setNewComment('');
   };
 
   return (
